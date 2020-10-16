95ca7ab7d731bc34d4f5f9b12460eab6652ebded
saleheen.noor <saleheen.noor@gmail.com>
2020-10-16 23:52:33 +0500

    Minor Tweaks
    Minor Tweaks

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
+document.getElementById('chat_app').style.display = 'none';
 var getMsg = document.getElementById('msg');
 var sendBtn = document.getElementById('send_btn');
 
+let login = () => {
+    document.getElementById('login_page').style.display = 'none';
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var user = result.user;
+        let userData = {
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+            uid: user.uid
+        }
+        firebase.database().ref('all_users/' + userData.uid).set(userData);
+    }).catch(function (error) {
+        alert(error.message)
+        document.getElementById('login_page').style.display = 'block';
+    });
+
+}
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user != null) {
+        document.getElementById('chat_app').style.display = 'block';
+        document.getElementById('login_page').style.display = 'none';
         var prevent_duplicate_db = false;
         firebase.database().ref('all_users').on('child_added', (data) => {
             if (data.val().uid !== firebase.auth().currentUser.uid) {
@@ -28,26 +49,30 @@ firebase.auth().onAuthStateChanged(function (user) {
                 createFrndList.appendChild(createFrndName)
 
                 let createFrndMsgSnap = document.createElement('p');
-                createFrndMsgSnap.setAttribute('id','snap' + data.val().uid);
+                createFrndMsgSnap.setAttribute('id', 'snap' + data.val().uid);
                 createFrndMsgSnap.setAttribute('class', 'friendMsgSnap');
                 createFrndMsgSnap.innerHTML = '';
                 createFrndList.appendChild(createFrndMsgSnap)
+
+
+                document.getElementById('user_profile_image').src = firebase.auth().currentUser.photoURL;
+
             }
             if (prevent_duplicate_db === false) {
-            }
-            else {
                 document.getElementById('user_profile_image').src = firebase.auth().currentUser.photoURL;
             }
+
         })
     }
     else {
         document.getElementById('user_profile_image').src = "images/user_profile_image.png";
-        document.getElementById('friendList').innerHTML = ''
-}
+        document.getElementById('friendList').innerHTML = '';
+    }
 });
 
 
 let chatKey;
+
 let start_Chat = (friendId, img, name) => {
     document.getElementById('msg-output').innerHTML = '';
     document.getElementById('user_name').innerHTML = name;
@@ -80,7 +105,6 @@ let start_Chat = (friendId, img, name) => {
         getChatMsgs(chatKey, img, friendId)
     })
     document.getElementById('chat_field').style.display = 'block';
-
 }
 
 
@@ -105,7 +129,7 @@ let getChatMsgs = (chatKey, img, friendId) => {
 
             let createTimeStamp = document.createElement('span');
             createTimeStamp.setAttribute('class', 'time-stamp')
-            createTimeStamp.innerHTML = data.val().time;
+            createTimeStamp.innerHTML += data.val().time;
 
             getMsgAreaDiv.appendChild(createMsgEle);
             createMsgEle.appendChild(createMsgUserImage);
@@ -137,7 +161,7 @@ let getChatMsgs = (chatKey, img, friendId) => {
             let createTimeStamp = document.createElement('span');
             createTimeStamp.setAttribute('class', 'time-stamp-other')
             createTimeStamp.style.cssFloat = 'right';
-            createTimeStamp.innerHTML = data.val().time;
+            createTimeStamp.innerHTML += data.val().time;
 
             getMsgAreaDiv.appendChild(createMsgEle);
             createMsgEle.appendChild(createMsgUserImage);
@@ -194,14 +218,6 @@ let = openMenu = () => {
     closeBtn.setAttribute('id', 'closeBtn')
     menuOpen.appendChild(closeBtn)
 
-    // var login = document.createElement('p')
-    // loginLink = document.createElement('a')
-    // loginLink.setAttribute('href', '#')
-    // loginLink.setAttribute('onClick', 'login()')
-    // loginLink.innerHTML = "Login"
-    // login.appendChild(loginLink)
-    // menuOpen.appendChild(login)
-
     var logout = document.createElement('p')
     logoutLink = document.createElement('a')
     logoutLink.setAttribute('href', '#')
@@ -222,10 +238,9 @@ let = logout = () => {
         document.getElementById('closeBtn').parentNode.remove();
         document.getElementById('msg-output').innerHTML = '';
         document.getElementById('user_name').innerHTML = '';
-        window.location.replace("index.html");
-        // console.log('Sign-out successful.')
+        document.getElementById('chat_app').style.display = 'none';
+        document.getElementById('login_page').style.display = 'block';
     }).catch(function (error) {
-        console.log('An error happened.')
-        alert(error)
+        alert(error);
     });
 }
